bfd9a91ee4f2117b18e8694ad8b0894a19a37d58
Miguel Jaque <mjaque@migueljaque.com>
2023-10-18 06:23:26 +0200

    Creación de tarjetas.

--- script.js
+++ script.js
@@ -2,34 +2,49 @@ let colores = ['red', 'pink', 'orange', 'blue', 'green', 'yellow', 'purple', 'br
 
 class Juego{
 	#nivel = null
+	#tablero = null
+	#tarjetas = null
+	#primerTurno = null
+
 	constructor(tablero){
-		this.nivel = 3
-		this.tablero = tablero 
+		this.#nivel = 1
+		this.#tablero = tablero 
+		this.#primerTurno = true
 		this.crearTablero()
 	}
 	crearTablero(){
-		let tarjetas = []
-		let numTarjetas = 6 + this.nivel * 2
+		this.#tarjetas = []
+		let numTarjetas = 6 + this.#nivel * 2
 		let numCols = Math.round(Math.sqrt(numTarjetas * 3/2 ))
-		this.tablero.style.gridTemplateColumns = `repeat(${numCols}, auto)`
+		this.#tablero.style.gridTemplateColumns = `repeat(${numCols}, auto)`
 
 		let coloresAleatorios = colores.toSorted( (a, b) => {return Math.random() - 0.5} )
 
-		while(this.tablero.firstElementChild)
-			this.tablero.firstElementChild.remove()
+		while(this.#tablero.firstElementChild)
+			this.#tablero.firstElementChild.remove()
 
 		for (let i = 0; i < numTarjetas; i += 2){
 			let div = document.createElement('div')
-			div.color = coloresAleatorios[i]
-			//div.classList.add('oculto')
-			console.log(div.color)
+			div.setAttribute('color', coloresAleatorios[i])
+			div.classList.add('oculto')
 			div.style.backgroundColor = div.color
-			tarjetas.push(div)
-			tarjetas.push(div.cloneNode())
+			this.#tarjetas.push(div)
+			this.#tarjetas.push(div.cloneNode())
 		}
-		tarjetas.sort( (a, b) => {return Math.random() - 0.5} )
-		tarjetas.forEach( tarjeta => {tablero.appendChild(tarjeta)})
+		this.#tarjetas.sort( (a, b) => {return Math.random() - 0.5} )
+		this.#tarjetas.forEach( tarjeta => {
+			tarjeta.onclick = this.descubrir.bind(this)
+			this.#tablero.appendChild(tarjeta)})
 
+	}
+	descubrir(evento){
+		let tarjeta = evento.target
+	console.log(tarjeta)
+		tarjeta.classList.remove('oculto')
+		tarjeta.style.backgroundColor = tarjeta.getAttribute('color')
+		if (!this.#primerTurno)
+			this.#tarjetas.forEach( tarjeta => { tarjeta.classList.add('oculto') })
+		this.#primerTurno = !this.#primerTurno
 	}
 }
 
